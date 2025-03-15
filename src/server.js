e1cd5ced6750b71ff37f4e7b6209e9cf8a951158
709macdonald <709macdonald@gmail.com>
2025-03-14 21:50:48 -0230

    Updated my server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
+const express = require("express");
+const sequelize = require("./config/database");
+const User = require("./models/user");
 
+const app = express();
+const port = 5005;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Unable to sync database:", error);
+  });
