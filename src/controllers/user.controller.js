c1a35cc4eb5216754718893f1ed3c40ad24ccb94
709macdonald <709macdonald@gmail.com>
2025-04-12 13:30:43 -0230

    updated outline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/user.model");
 const { generateToken, verifyToken } = require("../utils/auth.utils");
 const bcrypt = require("bcrypt");
 
