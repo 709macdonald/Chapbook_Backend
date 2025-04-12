655c1189fe90c49c7408951c7a0ef1046c8a11e1
709macdonald <709macdonald@gmail.com>
2025-04-12 13:32:30 -0230

    fixed path to model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../models/user");
 const { generateToken, verifyToken } = require("../utils/auth.utils");
 const bcrypt = require("bcrypt");
 
